c4560c2868d5a9c37d007c8a84b517fda75b4a78
sahil <sahil311289@gmail.com>
2018-06-02 17:19:48 -0700

    Tree traversal

---
+++ src/HieghtOfTreeRecursive.java
@@ -0,0 +1,19 @@
+public class HieghtOfTreeRecursive {
+    public static void main(String[] args) {
+        Tree t = new Tree();
+        Node root = t.createTree2();
+
+        System.out.println(checkHeight(root));
+    }
+
+    private static int checkHeight(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftHeight = checkHeight(root.left);
+        int rightHeight = checkHeight(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
